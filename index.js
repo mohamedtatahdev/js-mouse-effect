bd5733ca2f1bca596cf6015fbb3cd0e9ed929bd3
mohamedtatahdev <mohamed.tatah69@gmail.com>
2024-04-18 20:29:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const mouses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+    mouses.forEach((mouse) => {
+        mouse.style.top = e.y + "px";
+        mouse.style.left = e.x + "px";
+        console.log(mouse)
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+// window.addEventListener("mousemove", (e) => {
+//     cursor.style.top = e.y + "px";
+//     cursor.style.left = e.x + "px";
+
+//     mouse1.style.top = e.y + "px";
+//     mouse1.style.left = e.x + "px";
+
+//     mouse2.style.top = e.y + "px";
+//     mouse2.style.left = e.x + "px";
+
+// })
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
